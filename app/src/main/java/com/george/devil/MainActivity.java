99398159ae1b08e5a19d7ec692a0ed236619da33
george <liggidarck@gmail.com>
2020-10-24 10:02:40 +0300

    Добавлены фрагменты главная, заметки, сообщения, профиль.

--- app/src/main/java/com/george/devil/MainActivity.java
+++ app/src/main/java/com/george/devil/MainActivity.java
@@ -1,14 +1,64 @@
 package com.george.devil;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new fragmentHome()).commit();
+        }
+
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    Log.i(TAG, "Fragment home started");
+                    selectedFragment = new fragmentHome();
+                    break;
+                case R.id.nav_note:
+                    Log.i(TAG, "Fragment note started");
+                    selectedFragment = new fragmentNote();
+                    break;
+                case R.id.nav_messeg:
+                    Log.i(TAG, "Fragment messege started");
+                    selectedFragment = new fragmentMessege();
+                    break;
+                case R.id.nav_profile:
+                    Log.i(TAG, "Fragment porofile started");
+                    selectedFragment = new fragmentProfile();
+                    break;
+            }
+            assert selectedFragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+        }
+    };
+
 }
