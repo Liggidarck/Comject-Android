7d110b1838ae1f76812a95eacd4d67c9a7236ae1
George Filatov <liggidarck@gmail.com>
2020-11-28 21:02:35 +0300

    Code formated

--- app/src/main/java/com/george/devil/MainActivity.java
+++ app/src/main/java/com/george/devil/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,41 +25,40 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, new fragmentHome()).commit();
-        }
+        if (savedInstanceState == null)
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new fragmentHome()).commit();
 
     }
 
+    @SuppressLint("NonConstantResourceId")
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment selectedFragment = null;
-            switch (item.getItemId()) {
-                case R.id.nav_home:
-                    Log.i(TAG, "Fragment home started");
-                    selectedFragment = new fragmentHome();
-                    break;
-                case R.id.nav_note:
-                    Log.i(TAG, "Fragment note started");
-                    selectedFragment = new fragmentNote();
-                    break;
-                case R.id.nav_messeg:
-                    Log.i(TAG, "Fragment messege started");
-                    selectedFragment = new fragmentMessege();
-                    break;
-                case R.id.nav_profile:
-                    Log.i(TAG, "Fragment porofile started");
-                    selectedFragment = new fragmentProfile();
-                    break;
-            }
-            assert selectedFragment != null;
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            item -> {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        Log.i(TAG, "Fragment home started");
+                        selectedFragment = new fragmentHome();
+                        break;
+                    case R.id.nav_note:
+                        Log.i(TAG, "Fragment note started");
+                        selectedFragment = new fragmentNote();
+                        break;
+                    case R.id.nav_explore:
+                        Log.i(TAG, "Fragment explore started");
+                        selectedFragment = new fragmentExplore();
+                        break;
+                    case R.id.nav_messeg:
+                        Log.i(TAG, "Fragment messege started");
+                        selectedFragment = new fragmentMessege();
+                        break;
+                    case R.id.nav_profile:
+                        Log.i(TAG, "Fragment porofile started");
+                        selectedFragment = new fragmentProfile();
+                        break;
+                }
+                assert selectedFragment != null;
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
 
-            return true;
-        }
-    };
-
+                return true;
+            };
 }
